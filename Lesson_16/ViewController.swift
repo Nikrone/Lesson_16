e0424b5b5dabb24a3070d13e757daecdbd14bc66
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-09-08 20:01:42 +0300

    add part-1 Lesson 16

--- Lesson_16/ViewController.swift
+++ Lesson_16/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    //      UserDefaults - используется для сохранения ПРОСТЫХ данных!
+    //      UserDefaults - сохраняет даные в ОТДЕЛЬНОМ файле!
+    //      Keychain - используется для сохранения ВАЖНЫХ данных (данные авторизации и т.д)!
+    
+    let key = "TextFieldTextKey"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //        сохранение данных
+        let value = UserDefaults.standard.string(forKey: key)
+        //        сохранение данных с типом Any
+        //        let value = UserDefaults.standard.value(forKey: key) as? String
+        //         чтение данных структуры (unwrap значения)
+                let string = UserDefaults.standard.string(forKey: key) ?? ""
+        //        JSONDecoder - преобразовывает обратно
+                let profile  =  try? JSONDecoder().decode(Profile.self, from: Data(base64Encoded: string) ?? Data())
+        
+        textField.text = profile?.name
+        
+        //       удаление данных из UserDefaults
+        UserDefaults.standard.removeObject(forKey: key)
+        
+
     }
+    
+    //    сохранение данных по нажатию на кнопку
+    @IBAction func buttonPressed() {
+        let  text = textField.text ?? ""
+        
+//        сохранение данных структуры
+        let profile = Profile(name: text, surname: text)
+//        JSONEncoder - преобразовывает в формат JSON
+        let string = try? JSONEncoder().encode(profile).base64EncodedString()
+        UserDefaults.standard.setValue(text, forKey: key)
+        UserDefaults.standard.synchronize()
+        
+    }
+    
+}
 
-
+//  сохранение структуры (конформит протоколу - Codable(преобразовывает в строку))
+struct Profile: Codable {
+    let name: String
+    let surname: String
 }
 
